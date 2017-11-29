7191ad7f045c833768fd69f76fb3015d08acccac
vaclavlangr <vendavenda3@gmail.com>
2017-11-29 09:30:50 +0100

    Bad version of simple calculator app

---
+++ Bad version/CalculatorApp/src/sample/Calculator.java
@@ -0,0 +1,117 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Control;
+import javafx.scene.control.TextField;
+
+public class Calculator {
+
+    public TextField operandInput;
+    private Float lastOperand = null;
+    private Operation selectedOperation;
+    private boolean dotFlag = false;
+
+    public void numberPressed(ActionEvent actionEvent) {
+        if(operandInput.getText().equals("NaN")) {
+            operandInput.setText("");
+        }
+        String stringId = ((Control)actionEvent.getSource()).getId();
+        operandInput.setText(operandInput.getText() + stringId);
+    }
+
+    private Float doOperation() {
+        if(lastOperand == null) {
+            return 0.0f;
+        }
+        if(operandInput.getText().equals("")) {
+            return Float.NaN;
+        }
+        Float result = lastOperand;
+        switch (selectedOperation) {
+            case Add:
+                result += Float.parseFloat(operandInput.getText());
+                break;
+            case Sub:
+                result -= Float.parseFloat(operandInput.getText());
+                break;
+            case Multiply:
+                result *= Float.parseFloat(operandInput.getText());
+                break;
+            case Divide:
+                if(Float.parseFloat(operandInput.getText()) == 0.0) {
+                    return Float.NaN;
+                }
+                result /= Float.parseFloat(operandInput.getText());
+                break;
+        }
+        return result;
+    }
+
+    public void equalTrigger() {
+        Float value = doOperation();
+        operandInput.setText(value.toString());
+        dot = false;
+        lastOperand = null;
+    }
+
+    public void dotTrigger() {
+        if(!dot) {
+            dot = true;
+            operandInput.setText(operandInput.getText() + ".");
+        }
+    }
+
+    public void divide() {
+        if(lastOperand == null) {
+            lastOperand = Float.parseFloat(operandInput.getText());
+        } else {
+            if(operandInput.getText().length() > 0) {
+                lastOperand = doOperation();
+            }
+        }
+        dot = false;
+        operandInput.setText("");
+        selectedOperation = Operation.Divide;
+    }
+
+    public void multiply() {
+        if(lastOperand == null) {
+            lastOperand = Float.parseFloat(operandInput.getText());
+        } else {
+            if(operandInput.getText().length() > 0) {
+                lastOperand = doOperation();
+            }
+        }
+        dot = false;
+        operandInput.setText("");
+        selectedOperation = Operation.Multiply;
+    }
+
+    public void sub() {
+        if(lastOperand == null) {
+            if(operandInput.getText().length() > 0) {
+                lastOperand = Float.parseFloat(operandInput.getText());
+            } else {
+                lastOperand = 0.0f;
+            }
+        } else {
+            lastOperand = doOperation();
+        }
+        dot = false;
+        operandInput.setText("");
+        selectedOperation = Operation.Sub;
+    }
+
+    public void add() {
+        if(lastOperand == null) {
+            lastOperand = Float.parseFloat(operandInput.getText());
+        } else {
+            if(operandInput.getText().length() > 0) {
+                lastOperand = doOperation();
+            }
+        }
+        dot = false;
+        operandInput.setText("");
+        selectedOperation = Operation.Add;
+    }
+}
---
+++ Bad version/CalculatorApp/src/sample/Operation.java
@@ -0,0 +1,8 @@
+package sample;
+
+/**
+ * Created by vaclavlangr on 29.11.2017.
+ */
+public enum  Operation {
+    Add, Sub, Multiply, Divide
+}
